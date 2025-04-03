08b05de7b3e9c6d0e205c9aeff4bec9e7fa5967c
Mohammed Laouina <simed.laouina@gmail.com>
2025-04-03 15:16:07 +0000

    creation de supplier et KStream

--- src/main/java/com/example/kafka_spring_cloud_stream/handlers/PageEventHandler.java
+++ src/main/java/com/example/kafka_spring_cloud_stream/handlers/PageEventHandler.java
@@ -1,13 +1,21 @@
 package com.example.kafka_spring_cloud_stream.handlers;
 
 import com.example.kafka_spring_cloud_stream.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.BridgeFrom;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -33,4 +41,22 @@ public class PageEventHandler {
             );
         };
     }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStream(){
+        return (stream)->
+                stream
+                        //.filter((k,v)->v.duration()>100)
+                        .map((k,v)->new KeyValue<>(v.name(), 0L))
+                        .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                        .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                        //.aggregate(()->0.0, (k,v,total)->total+v, Materialized.as("total-store"))
+                        .count(Materialized.as("count-store"))
+                        .toStream()
+                        .map((k,v)->new KeyValue<>(k.key(), v))
+                ;
+
+    }
+    
+
 }
